17354cb308328695f5e323e32e53e9d6d9358d7b
Dishank Patil <dishankjp05@gmail.com>
2024-10-17 22:03:04 +0530

    intial commit

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,47 @@
+import { Input } from "postcss";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+export const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [loading, setLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") setVariant("REGISTER");
+    else setVariant("LOGIN");
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = () => {
+    setLoading(true);
+    if (variant === "REGISTER") {
+    }
+    if (variant === "LOGIN") {
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setLoading(true);
+  };
+
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='bg-slate-100 px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}></form>
+      </div>
+    </div>
+  );
+};
